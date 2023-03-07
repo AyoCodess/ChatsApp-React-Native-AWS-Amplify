cf98dc3ecdbd34b36bb005508111d506e4b0754e
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-03-07 06:42:20 +0100

    added react navigation types

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,18 +1,18 @@
 import React, { ReactNode } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { View, Text } from "react-native";
-import { ChatScreen } from "../screens";
+import { ChatScreen, ChatScreens } from "../screens";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./types";
 
 export function Navigation() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="chat_screens" component={ChatScreens} />
+        <Stack.Screen name="chat_screen" component={ChatScreen} />
       </Stack.Navigator>
-      <ChatScreen />
     </NavigationContainer>
   );
 }
